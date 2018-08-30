d348c5e7ff03175556830129c11416f978950b4e
Ubuntu <ubuntu@p01r03srv19.maas>
2018-08-30 16:09:20 +0000

    Test

--- main.cpp
+++ main.cpp
@@ -5,14 +5,13 @@
 #include <QCommandLineParser>
 #include <QCommandLineOption>
 
-
 #include <iostream>
 #include <unistd.h>
 #include <stdio.h>
 
 #include <cuda_profiler_api.h>
 
-#include "MainWindow.hpp"
+// #include "MainWindow.hpp"
 #include "DomainData.hpp"
 #include "SimulationThread.hpp"
 #include "CFDWidget.hpp"
@@ -23,55 +22,55 @@ cudaStream_t renderStream = 0;
 
 int main(int argc, char **argv)
 {
-  // cudaProfilerStart();
-  // // CUDA stream priorities. Simulation has highest priority, rendering lowest.
-  // int priorityHigh, priorityLow;
-  // cudaDeviceGetStreamPriorityRange(&priorityLow, &priorityHigh);
-  // cudaStreamCreateWithPriority(&simStream, cudaStreamNonBlocking, priorityHigh);
-  // // cudaStreamCreateWithPriority(&renderStream, cudaStreamNonBlocking, priorityLow);
+  cudaProfilerStart();
+  // CUDA stream priorities. Simulation has highest priority, rendering lowest.
+  int priorityHigh, priorityLow;
+  cudaDeviceGetStreamPriorityRange(&priorityLow, &priorityHigh);
+  cudaStreamCreateWithPriority(&simStream, cudaStreamNonBlocking, priorityHigh);
+  // cudaStreamCreateWithPriority(&renderStream, cudaStreamNonBlocking, priorityLow);
 
-  // simThread = new SimulationThread(new DomainData());
-  // simThread->setSchedulePriority(OpenThreads::Thread::ThreadPriority ::THREAD_PRIORITY_MIN);
-
-  // QApplication app(argc, argv);
-
-  // MainWindow window;
-  // // CFDWidget *widget = new CFDWidget(simThread, 1, 1, &window);
-  // // window.setCentralWidget(widget);
-  // window.show();
-  // window.resize(QDesktopWidget().availableGeometry(&window).size() * 0.5);
-  // // widget->setFocus();
-
-  // // simThread->start();
-
-  // QObject::connect(&app, SIGNAL(aboutToQuit()), &window, SLOT(closing()));
-  // const int retval = app.exec();
-
-  // cudaProfilerStop();
-  // simThread->cancel();
-  // simThread->join();
-  // cudaStreamSynchronize(0);
-  // cudaDeviceSynchronize();
-  // cudaDeviceReset();
-
-  // return retval;
-
-  Q_INIT_RESOURCE(application);
+  simThread = new SimulationThread(new DomainData());
+  simThread->setSchedulePriority(OpenThreads::Thread::ThreadPriority ::THREAD_PRIORITY_MIN);
 
   QApplication app(argc, argv);
-  QCoreApplication::setOrganizationName("QtProject");
-  QCoreApplication::setApplicationName("Application Example");
-  QCoreApplication::setApplicationVersion(QT_VERSION_STR);
-  QCommandLineParser parser;
-  parser.setApplicationDescription(QCoreApplication::applicationName());
-  parser.addHelpOption();
-  parser.addVersionOption();
-  parser.addPositionalArgument("file", "The file to open.");
-  parser.process(app);
 
-  MainWindow mainWin;
-  if (!parser.positionalArguments().isEmpty())
-    mainWin.loadFile(parser.positionalArguments().first());
-  mainWin.show();
-  return app.exec();
+  QMainWindow window;
+  CFDWidget *widget = new CFDWidget(simThread, 1, 1, &window);
+  window.setCentralWidget(widget);
+  window.show();
+  window.resize(QDesktopWidget().availableGeometry(&window).size() * 0.5);
+  widget->setFocus();
+
+  simThread->start();
+
+  QObject::connect(&app, SIGNAL(aboutToQuit()), &window, SLOT(closing()));
+  const int retval = app.exec();
+
+  cudaProfilerStop();
+  simThread->cancel();
+  simThread->join();
+  cudaStreamSynchronize(0);
+  cudaDeviceSynchronize();
+  cudaDeviceReset();
+
+  return retval;
+
+  // Q_INIT_RESOURCE(application);
+
+  // QApplication app(argc, argv);
+  // QCoreApplication::setOrganizationName("QtProject");
+  // QCoreApplication::setApplicationName("Application Example");
+  // QCoreApplication::setApplicationVersion(QT_VERSION_STR);
+  // QCommandLineParser parser;
+  // parser.setApplicationDescription(QCoreApplication::applicationName());
+  // parser.addHelpOption();
+  // parser.addVersionOption();
+  // parser.addPositionalArgument("file", "The file to open.");
+  // parser.process(app);
+
+  // MainWindow mainWin;
+  // if (!parser.positionalArguments().isEmpty())
+  //   mainWin.loadFile(parser.positionalArguments().first());
+  // mainWin.show();
+  // return app.exec();
 }
