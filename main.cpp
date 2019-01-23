de642b8405e8925163f3567c877ee7aade121eb8
Ubuntu <ubuntu@p01r03srv19.maas>
2019-01-23 13:33:56 +0000

    Fix num requested devices option

--- main.cpp
+++ main.cpp
@@ -16,16 +16,16 @@
 #include "MainWindow.hpp"
 #include "SimulationWorker.hpp"
 
-QCoreApplication *createApplication(int &argc, char *argv[]) {
-  for (int i = 1; i < argc; ++i)
+QCoreApplication *createApplication(int *argc, char *argv[]) {
+  for (int i = 1; i < *argc; ++i)
     if (!qstrcmp(argv[i], "-n") || !qstrcmp(argv[i], "--no-gui"))
-      return new QCoreApplication(argc, argv);
-  return new QApplication(argc, argv);
+      return new QCoreApplication(*argc, argv);
+  return new QApplication(*argc, argv);
 }
 
 int main(int argc, char **argv) {
   Q_INIT_RESOURCE(res);
-  QScopedPointer<QCoreApplication> appPtr(createApplication(argc, argv));
+  QScopedPointer<QCoreApplication> appPtr(createApplication(&argc, argv));
 
   QCoreApplication::setOrganizationName("RISE SICS North");
   QCoreApplication::setApplicationName("LUA LBM GPU Leeds Luleå 2018");
@@ -40,7 +40,7 @@ int main(int argc, char **argv) {
   QCommandLineOption geometryOpt({"g", "geometry"}, "Lua LBM geometry script.",
                                  "geometry.lua");
   QCommandLineOption devicesOpt({"d", "devices"},
-                                "Number of CUDA devices to use.", "8");
+                                "Number of CUDA devices to use.", "1");
   QCommandLineOption headlessOpt({"n", "no-gui"}, "Run in headless mode");
   QCommandLineOption iterationsOpt(
       {"i", "iterations"},
@@ -57,12 +57,12 @@ int main(int argc, char **argv) {
   QString geometryFilePath = parser.value("geometry");
   int iterations = parser.value("iterations").toInt();
 
-  int numFoundDevices;
+  // Check that requested number of CUDA devices exist
+  int numDevices, numRequestedDevices, numFoundDevices;
   CUDA_RT_CALL(cudaGetDeviceCount(&numFoundDevices));
   std::cout << "Found " << numFoundDevices << " CUDA GPU(s)" << std::endl;
-  // numFoundDevices = min(8, numFoundDevices);
-  int numRequestedDevices = parser.value("devices").toInt();
-  int numDevices;
+  numRequestedDevices = parser.value("devices").toInt();
+  if (numRequestedDevices == 0) numRequestedDevices = numFoundDevices;
   if (numRequestedDevices <= numFoundDevices) {
     numDevices = numRequestedDevices;
   } else {
@@ -70,18 +70,14 @@ int main(int argc, char **argv) {
               << " available" << std::endl;
     return 1;
   }
-
+  std::cout << "Using " << numDevices << " CUDA GPU(s)" << std::endl;
   CUDA_RT_CALL(cudaProfilerStart());
-
   CUDA_RT_CALL(cudaSetDevice(0));
   CUDA_RT_CALL(cudaFree(0));
-  int cudaDev;
-  CUDA_RT_CALL(cudaGetDevice(&cudaDev));
-  std::cout << "Using device " << cudaDev << std::endl;
 
+  // Load LUA scripts if provided
   DomainData *domainData = new DomainData(numDevices);
   SimulationWorker *simWorker = new SimulationWorker(NULL, iterations);
-
   if (!settingsFilePath.isEmpty() && !geometryFilePath.isEmpty()) {
     domainData->loadFromLua(geometryFilePath.toUtf8().constData(),
                             settingsFilePath.toUtf8().constData());
@@ -99,6 +95,7 @@ int main(int argc, char **argv) {
 
   int retval;
   if (headless) {
+    // Use console client
     QCoreApplication *app = qobject_cast<QCoreApplication *>(appPtr.data());
     ConsoleClient *client = new ConsoleClient(simWorker, numDevices, app);
     QObject::connect(app, SIGNAL(aboutToQuit()), client, SLOT(close()));
@@ -107,6 +104,7 @@ int main(int argc, char **argv) {
     retval = app->exec();
 
   } else {
+    // Use QT client
     QApplication *app = qobject_cast<QApplication *>(appPtr.data());
     MainWindow window(simWorker, numDevices);
     QObject::connect(app, SIGNAL(aboutToQuit()), &window, SLOT(close()));
@@ -114,6 +112,8 @@ int main(int argc, char **argv) {
     window.resize(QDesktopWidget().availableGeometry(&window).size() * 0.5);
     retval = app->exec();
   }
+
+  // Reset devices and exit
 #pragma omp parallel num_threads(numDevices)
   {
     const int dev = omp_get_thread_num();
@@ -122,8 +122,6 @@ int main(int argc, char **argv) {
     CUDA_RT_CALL(cudaDeviceReset());
   }
   CUDA_RT_CALL(cudaProfilerStop());
-
   std::cout << "Exited with " << retval << std::endl;
-
   return retval;
 }
