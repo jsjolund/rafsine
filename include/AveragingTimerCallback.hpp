4336330371abd70afb0e94adfcba8962d29141fc
Ubuntu <ubuntu@p01r03srv19.maas>
2019-03-25 08:37:19 +0000

    Check for no output csv defined

--- include/AveragingTimerCallback.hpp
+++ include/AveragingTimerCallback.hpp
@@ -43,28 +43,31 @@ class AveragingTimerCallback : public SimulationTimerCallback {
         m_lastTicks(0),
         m_avgAreas(avgAreas),
         m_avgs(avgAreas.size()) {
-    QFile outputCSV(m_outputCSVPath);
-    QFileInfo outputCSVinfo(outputCSV);
-    if (outputCSVinfo.size() == 0 &&
-        outputCSV.open(QIODevice::WriteOnly | QIODevice::Append)) {
-      QTextStream stream(&outputCSV);
-      stream << "time,";
-      for (int i = 0; i < m_avgs.size(); i++) {
-        VoxelArea avgArea = m_avgAreas.at(i);
-        QString name = QString::fromStdString(avgArea.getName());
-        stream << name << "_T," << name << "_Q";
-        if (i == m_avgs.size() - 1)
-          stream << endl;
-        else
-          stream << ",";
+    if (m_outputCSVPath.length() > 0) {
+      QFile outputCSV(m_outputCSVPath);
+      QFileInfo outputCSVinfo(outputCSV);
+      if (outputCSVinfo.size() == 0 &&
+          outputCSV.open(QIODevice::WriteOnly | QIODevice::Append)) {
+        QTextStream stream(&outputCSV);
+        stream << "time,";
+        for (int i = 0; i < m_avgs.size(); i++) {
+          VoxelArea avgArea = m_avgAreas.at(i);
+          QString name = QString::fromStdString(avgArea.getName());
+          stream << name << "_T," << name << "_Q";
+          if (i == m_avgs.size() - 1)
+            stream << endl;
+          else
+            stream << ",";
+        }
+      } else {
+        throw std::runtime_error("Failed to open output CSV file");
       }
-    } else {
-      throw std::runtime_error("Failed to open output CSV file");
     }
   }
 
   void run(uint64_t ticks) {
-    if (m_avgAreas.size() == 0) return;
+    if (m_avgAreas.size() == 0 || m_outputCSVPath.length() == 0) return;
+
     const uint64_t deltaTicks = ticks - m_lastTicks;
     m_lastTicks = ticks;
     QFile outputCSV(m_outputCSVPath);
