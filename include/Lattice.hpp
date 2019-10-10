5c21faec1f89b6d6875ab210cb5acd637b7cb012
Ubuntu <ubuntu@p01r03srv19.maas>
2019-10-10 10:18:24 +0200

    Add some docs

--- include/Lattice.hpp
+++ include/Lattice.hpp
@@ -13,6 +13,9 @@
 #include "Eigen/Geometry"
 #include "Partition.hpp"
 
+/**
+ * @brief An LBM lattice partitioned to be distributed between multiple GPUs
+ */
 class Lattice {
  protected:
   //! The size of the entire lattice
@@ -32,7 +35,6 @@ class Lattice {
  public:
   /**
    * @brief Get the neighbouring partition in a certain direction.
-   *
    * @param partition
    * @param direction
    * @return Partition
@@ -42,30 +44,83 @@ class Lattice {
     Eigen::Vector3i partPos = m_partitionPositions[partition];
     return getPartition(partPos + direction);
   }
+  /**
+   * @brief Get the neighbouring partition in a certain direction.
+   * @param partition
+   * @param direction
+   * @return Partition
+   */
   inline Partition getNeighbour(Partition partition, D3Q7::Enum direction) {
     return getNeighbour(partition, D3Q27[direction]);
   }
-
+  /**
+   * @brief Get the ghost layer parameters for copying between GPUs using
+   * cudaMemcpy2D()
+   *
+   * @param partition
+   * @param neighbour
+   * @param direction
+   * @return GhostLayerParameters
+   */
   GhostLayerParameters getGhostLayer(Partition partition, Partition neighbour,
                                      D3Q7::Enum direction) {
     return m_segments[partition][neighbour].at(direction);
   }
-
+  /**
+   * @brief Get a list of all partitions in this lattice
+   *
+   * @return std::vector<Partition>
+   */
   inline std::vector<Partition> getPartitions() const { return m_partitions; }
+  /**
+   * @brief Get the size/extents of the lattice in 3D
+   *
+   * @return Eigen::Vector3i
+   */
   inline Eigen::Vector3i getExtents() const { return m_latticeSize; }
+  /**
+   * @brief Get the total number of lattice sites
+   *
+   * @return size_t
+   */
   inline size_t getSize() const {
     return m_latticeSize.x() * m_latticeSize.y() * m_latticeSize.z();
   }
+  /**
+   * @brief Get an integer vector representing how many times the lattice was
+   * partitioned/divided along each 3D axis
+   *
+   * @return Eigen::Vector3i
+   */
   inline Eigen::Vector3i getNumPartitions() const { return m_partitionCount; }
+  /**
+   * @brief Get the total number of lattice partitions
+   *
+   * @return int
+   */
   inline int getNumPartitionsTotal() const { return m_partitions.size(); }
 
   Lattice(unsigned int latticeSizeX, unsigned int latticeSizeY,
           unsigned int latticeSizeZ, unsigned int subdivisions = 1,
           unsigned int ghostLayerSize = 0);
-
+  /**
+   * @brief Get the partition containing the 3D lattice coorindate
+   *
+   * @param x
+   * @param y
+   * @param z
+   * @return Partition
+   */
   Partition getPartitionContaining(unsigned int x, unsigned int y,
                                    unsigned int z) const;
-
+  /**
+   * @brief Get a specific partition by its position in the domain decomposition
+   *
+   * @param x Integer between 0 and less than number of divisions along x-axis
+   * @param y Integer between 0 and less than number of divisions along y-axis
+   * @param z Integer between 0 and less than number of divisions along z-axis
+   * @return Partition
+   */
   inline Partition getPartition(int x = 0, int y = 0, int z = 0) const {
     // Periodic
     x = x % m_partitionCount.x();
