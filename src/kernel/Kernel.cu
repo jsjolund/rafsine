7b70554f39cc8ee81b196061047329457efd51a6
Ubuntu <ubuntu@p01r03srv19.maas>
2019-11-01 17:18:07 +0100

    Refactor lbm kernel code

--- src/kernel/Kernel.cu
+++ src/kernel/Kernel.cu
@@ -33,16 +33,6 @@ __device__ PhysicalQuantity compute(
     //  integrated in time (so /nbr_of_time_steps to get average)
     real* __restrict__ averageSrc,
     real* __restrict__ averageDst) {
-  real f0, f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11, f12, f13, f14, f15,
-      f16, f17, f18;
-  real f0eq, f1eq, f2eq, f3eq, f4eq, f5eq, f6eq, f7eq, f8eq, f9eq, f10eq, f11eq,
-      f12eq, f13eq, f14eq, f15eq, f16eq, f17eq, f18eq;
-  real f1diff, f2diff, f3diff, f4diff, f5diff, f6diff, f7diff, f8diff, f9diff,
-      f10diff, f11diff, f12diff, f13diff, f14diff, f15diff, f16diff, f17diff,
-      f18diff;
-  real T0, T1, T2, T3, T4, T5, T6;
-  real T0eq, T1eq, T2eq, T3eq, T4eq, T5eq, T6eq;
-
   const voxel_t voxelID = voxels[I3D(pos, size)];
   if (voxelID == -1) return {.rho = 0, .T = 0, .vx = 0, .vy = 0, .vz = 0};
 
@@ -72,33 +62,33 @@ __device__ PhysicalQuantity compute(
   // z minus 1
   const int zm = ((z - 1) % nz + nz) % nz;
 
-  f0 = df3D(0, x, y, z, nx, ny, nz);
-  f1 = df3D(1, xm, y, z, nx, ny, nz);
-  f2 = df3D(2, xp, y, z, nx, ny, nz);
-  f3 = df3D(3, x, ym, z, nx, ny, nz);
-  f4 = df3D(4, x, yp, z, nx, ny, nz);
-  f5 = df3D(5, x, y, zm, nx, ny, nz);
-  f6 = df3D(6, x, y, zp, nx, ny, nz);
-  f7 = df3D(7, xm, ym, z, nx, ny, nz);
-  f8 = df3D(8, xp, yp, z, nx, ny, nz);
-  f9 = df3D(9, xm, yp, z, nx, ny, nz);
-  f10 = df3D(10, xp, ym, z, nx, ny, nz);
-  f11 = df3D(11, xm, y, zm, nx, ny, nz);
-  f12 = df3D(12, xp, y, zp, nx, ny, nz);
-  f13 = df3D(13, xm, y, zp, nx, ny, nz);
-  f14 = df3D(14, xp, y, zm, nx, ny, nz);
-  f15 = df3D(15, x, ym, zm, nx, ny, nz);
-  f16 = df3D(16, x, yp, zp, nx, ny, nz);
-  f17 = df3D(17, x, ym, zp, nx, ny, nz);
-  f18 = df3D(18, x, yp, zm, nx, ny, nz);
+  real f0 = df3D(0, x, y, z, nx, ny, nz);
+  real f1 = df3D(1, xm, y, z, nx, ny, nz);
+  real f2 = df3D(2, xp, y, z, nx, ny, nz);
+  real f3 = df3D(3, x, ym, z, nx, ny, nz);
+  real f4 = df3D(4, x, yp, z, nx, ny, nz);
+  real f5 = df3D(5, x, y, zm, nx, ny, nz);
+  real f6 = df3D(6, x, y, zp, nx, ny, nz);
+  real f7 = df3D(7, xm, ym, z, nx, ny, nz);
+  real f8 = df3D(8, xp, yp, z, nx, ny, nz);
+  real f9 = df3D(9, xm, yp, z, nx, ny, nz);
+  real f10 = df3D(10, xp, ym, z, nx, ny, nz);
+  real f11 = df3D(11, xm, y, zm, nx, ny, nz);
+  real f12 = df3D(12, xp, y, zp, nx, ny, nz);
+  real f13 = df3D(13, xm, y, zp, nx, ny, nz);
+  real f14 = df3D(14, xp, y, zm, nx, ny, nz);
+  real f15 = df3D(15, x, ym, zm, nx, ny, nz);
+  real f16 = df3D(16, x, yp, zp, nx, ny, nz);
+  real f17 = df3D(17, x, ym, zp, nx, ny, nz);
+  real f18 = df3D(18, x, yp, zm, nx, ny, nz);
 
-  T0 = Tdf3D(0, x, y, z, nx, ny, nz);
-  T1 = Tdf3D(1, xm, y, z, nx, ny, nz);
-  T2 = Tdf3D(2, xp, y, z, nx, ny, nz);
-  T3 = Tdf3D(3, x, ym, z, nx, ny, nz);
-  T4 = Tdf3D(4, x, yp, z, nx, ny, nz);
-  T5 = Tdf3D(5, x, y, zm, nx, ny, nz);
-  T6 = Tdf3D(6, x, y, zp, nx, ny, nz);
+  real T0 = Tdf3D(0, x, y, z, nx, ny, nz);
+  real T1 = Tdf3D(1, xm, y, z, nx, ny, nz);
+  real T2 = Tdf3D(2, xp, y, z, nx, ny, nz);
+  real T3 = Tdf3D(3, x, ym, z, nx, ny, nz);
+  real T4 = Tdf3D(4, x, yp, z, nx, ny, nz);
+  real T5 = Tdf3D(5, x, y, zm, nx, ny, nz);
+  real T6 = Tdf3D(6, x, y, zp, nx, ny, nz);
 
   real* fs[19] = {&f0,  &f1,  &f2,  &f3,  &f4,  &f5,  &f6,  &f7,  &f8, &f9,
                   &f10, &f11, &f12, &f13, &f14, &f15, &f16, &f17, &f18};
@@ -106,22 +96,25 @@ __device__ PhysicalQuantity compute(
 
   if (bc.m_type == VoxelType::WALL) {
     // Half-way bounceback
-    real3 v =
+    const real3 v =
         make_float3(bc.m_velocity.x(), bc.m_velocity.y(), bc.m_velocity.z());
-    real3 n = make_float3(bc.m_normal.x(), bc.m_normal.y(), bc.m_normal.z());
+    const real3 n =
+        make_float3(bc.m_normal.x(), bc.m_normal.y(), bc.m_normal.z());
 // BC for velocity dfs
 #pragma unroll
     for (int i = 0; i < 19; i++) {
-      real3 ei = make_float3(D3Q27directions[i * 3], D3Q27directions[i * 3 + 1],
-                             D3Q27directions[i * 3 + 2]);
+      const real3 ei =
+          make_float3(D3Q27directions[i * 3], D3Q27directions[i * 3 + 1],
+                      D3Q27directions[i * 3 + 2]);
       if (dot(ei, n) > 0.0)
         *fs[i] = df3D(D3Q27directionsOpposite[i], x, y, z, nx, ny, nz);
     }
 // BC for temperature dfs
 #pragma unroll
     for (int i = 1; i < 7; i++) {
-      real3 ei = make_float3(D3Q27directions[i * 3], D3Q27directions[i * 3 + 1],
-                             D3Q27directions[i * 3 + 2]);
+      const real3 ei =
+          make_float3(D3Q27directions[i * 3], D3Q27directions[i * 3 + 1],
+                      D3Q27directions[i * 3 + 2]);
       if (dot(ei, n) > 0.0)
         *Ts[i] = Tdf3D(D3Q27directionsOpposite[i], x, y, z, nx, ny, nz);
     }
@@ -129,19 +122,21 @@ __device__ PhysicalQuantity compute(
              bc.m_type == VoxelType::INLET_RELATIVE ||
              bc.m_type == VoxelType::INLET_ZERO_GRADIENT) {
     // Inlet boundary condition
-    real3 v =
+    const real3 v =
         make_float3(bc.m_velocity.x(), bc.m_velocity.y(), bc.m_velocity.z());
-    real3 n = make_float3(bc.m_normal.x(), bc.m_normal.y(), bc.m_normal.z());
+    const real3 n =
+        make_float3(bc.m_normal.x(), bc.m_normal.y(), bc.m_normal.z());
 // BC for velocity dfs
 #pragma unroll
     for (int i = 0; i < 19; i++) {
-      real3 ei = make_float3(D3Q27directions[i * 3], D3Q27directions[i * 3 + 1],
-                             D3Q27directions[i * 3 + 2]);
-      real dot_vv = dot(v, v);
+      const real3 ei =
+          make_float3(D3Q27directions[i * 3], D3Q27directions[i * 3 + 1],
+                      D3Q27directions[i * 3 + 2]);
+      const real dot_vv = dot(v, v);
       if (dot(ei, n) > 0.0) {
-        real wi = D3Q19weights[i];
-        real rho = 1.0;
-        real dot_eiv = dot(ei, v);
+        const real wi = D3Q19weights[i];
+        const real rho = 1.0;
+        const real dot_eiv = dot(ei, v);
         // if the velocity is zero, use half-way bounceback instead
         if (length(v) == 0.0) {
           *fs[i] = df3D(D3Q27directionsOpposite[i], x, y, z, nx, ny, nz);
@@ -155,9 +150,10 @@ __device__ PhysicalQuantity compute(
 // BC for temperature dfs
 #pragma unroll
     for (int i = 1; i < 7; i++) {
-      real3 ei = make_float3(D3Q27directions[i * 3], D3Q27directions[i * 3 + 1],
-                             D3Q27directions[i * 3 + 2]);
-      real wi = D3Q7weights[i];
+      const real3 ei =
+          make_float3(D3Q27directions[i * 3], D3Q27directions[i * 3 + 1],
+                      D3Q27directions[i * 3 + 2]);
+      const real wi = D3Q7weights[i];
       if (dot(ei, n) > 0.0) {
         if (bc.m_type == VoxelType::INLET_CONSTANT) {
           *Ts[i] = real(wi * bc.m_temperature * (1.0 + 3.0 * dot(ei, v)));
@@ -181,94 +177,114 @@ __device__ PhysicalQuantity compute(
   }
 
   // Compute physical quantities
-  real rho = f0 + f1 + f2 + f3 + f4 + f5 + f6 + f7 + f8 + f9 + f10 + f11 + f12 +
-             f13 + f14 + f15 + f16 + f17 + f18;
-  real T = T0 + T1 + T2 + T3 + T4 + T5 + T6;
-  real vx = (1 / rho) * (f1 - f2 + f7 - f8 + f9 - f10 + f11 - f12 + f13 - f14);
-  real vy = (1 / rho) * (f3 - f4 + f7 - f8 - f9 + f10 + f15 - f16 + f17 - f18);
-  real vz =
+  const real rho = f0 + f1 + f2 + f3 + f4 + f5 + f6 + f7 + f8 + f9 + f10 + f11 +
+                   f12 + f13 + f14 + f15 + f16 + f17 + f18;
+  const real T = T0 + T1 + T2 + T3 + T4 + T5 + T6;
+  const real vx =
+      (1 / rho) * (f1 - f2 + f7 - f8 + f9 - f10 + f11 - f12 + f13 - f14);
+  const real vy =
+      (1 / rho) * (f3 - f4 + f7 - f8 - f9 + f10 + f15 - f16 + f17 - f18);
+  const real vz =
       (1 / rho) * (f5 - f6 + f11 - f12 - f13 + f14 + f15 - f16 - f17 + f18);
 
   // Compute the equilibrium distribution function
-  real sq_term = -1.5f * (vx * vx + vy * vy + vz * vz);
-  f0eq = rho * (1.f / 3.f) * (1 + sq_term);
-  f1eq = rho * (1.f / 18.f) * (1 + 3 * vx + 4.5f * vx * vx + sq_term);
-  f2eq = rho * (1.f / 18.f) * (1 - 3 * vx + 4.5f * vx * vx + sq_term);
-  f3eq = rho * (1.f / 18.f) * (1 + 3 * vy + 4.5f * vy * vy + sq_term);
-  f4eq = rho * (1.f / 18.f) * (1 - 3 * vy + 4.5f * vy * vy + sq_term);
-  f5eq = rho * (1.f / 18.f) * (1 + 3 * vz + 4.5f * vz * vz + sq_term);
-  f6eq = rho * (1.f / 18.f) * (1 - 3 * vz + 4.5f * vz * vz + sq_term);
-  f7eq = rho * (1.f / 36.f) *
-         (1 + 3 * (vx + vy) + 4.5f * (vx + vy) * (vx + vy) + sq_term);
-  f8eq = rho * (1.f / 36.f) *
-         (1 - 3 * (vx + vy) + 4.5f * (vx + vy) * (vx + vy) + sq_term);
-  f9eq = rho * (1.f / 36.f) *
-         (1 + 3 * (vx - vy) + 4.5f * (vx - vy) * (vx - vy) + sq_term);
-  f10eq = rho * (1.f / 36.f) *
-          (1 - 3 * (vx - vy) + 4.5f * (vx - vy) * (vx - vy) + sq_term);
-  f11eq = rho * (1.f / 36.f) *
-          (1 + 3 * (vx + vz) + 4.5f * (vx + vz) * (vx + vz) + sq_term);
-  f12eq = rho * (1.f / 36.f) *
-          (1 - 3 * (vx + vz) + 4.5f * (vx + vz) * (vx + vz) + sq_term);
-  f13eq = rho * (1.f / 36.f) *
-          (1 + 3 * (vx - vz) + 4.5f * (vx - vz) * (vx - vz) + sq_term);
-  f14eq = rho * (1.f / 36.f) *
-          (1 - 3 * (vx - vz) + 4.5f * (vx - vz) * (vx - vz) + sq_term);
-  f15eq = rho * (1.f / 36.f) *
-          (1 + 3 * (vy + vz) + 4.5f * (vy + vz) * (vy + vz) + sq_term);
-  f16eq = rho * (1.f / 36.f) *
-          (1 - 3 * (vy + vz) + 4.5f * (vy + vz) * (vy + vz) + sq_term);
-  f17eq = rho * (1.f / 36.f) *
-          (1 + 3 * (vy - vz) + 4.5f * (vy - vz) * (vy - vz) + sq_term);
-  f18eq = rho * (1.f / 36.f) *
-          (1 - 3 * (vy - vz) + 4.5f * (vy - vz) * (vy - vz) + sq_term);
+  const real sq_term = -1.5f * (vx * vx + vy * vy + vz * vz);
+  const real f0eq = rho * (1.f / 3.f) * (1 + sq_term);
+  const real f1eq =
+      rho * (1.f / 18.f) * (1 + 3 * vx + 4.5f * vx * vx + sq_term);
+  const real f2eq =
+      rho * (1.f / 18.f) * (1 - 3 * vx + 4.5f * vx * vx + sq_term);
+  const real f3eq =
+      rho * (1.f / 18.f) * (1 + 3 * vy + 4.5f * vy * vy + sq_term);
+  const real f4eq =
+      rho * (1.f / 18.f) * (1 - 3 * vy + 4.5f * vy * vy + sq_term);
+  const real f5eq =
+      rho * (1.f / 18.f) * (1 + 3 * vz + 4.5f * vz * vz + sq_term);
+  const real f6eq =
+      rho * (1.f / 18.f) * (1 - 3 * vz + 4.5f * vz * vz + sq_term);
+  const real f7eq =
+      rho * (1.f / 36.f) *
+      (1 + 3 * (vx + vy) + 4.5f * (vx + vy) * (vx + vy) + sq_term);
+  const real f8eq =
+      rho * (1.f / 36.f) *
+      (1 - 3 * (vx + vy) + 4.5f * (vx + vy) * (vx + vy) + sq_term);
+  const real f9eq =
+      rho * (1.f / 36.f) *
+      (1 + 3 * (vx - vy) + 4.5f * (vx - vy) * (vx - vy) + sq_term);
+  const real f10eq =
+      rho * (1.f / 36.f) *
+      (1 - 3 * (vx - vy) + 4.5f * (vx - vy) * (vx - vy) + sq_term);
+  const real f11eq =
+      rho * (1.f / 36.f) *
+      (1 + 3 * (vx + vz) + 4.5f * (vx + vz) * (vx + vz) + sq_term);
+  const real f12eq =
+      rho * (1.f / 36.f) *
+      (1 - 3 * (vx + vz) + 4.5f * (vx + vz) * (vx + vz) + sq_term);
+  const real f13eq =
+      rho * (1.f / 36.f) *
+      (1 + 3 * (vx - vz) + 4.5f * (vx - vz) * (vx - vz) + sq_term);
+  const real f14eq =
+      rho * (1.f / 36.f) *
+      (1 - 3 * (vx - vz) + 4.5f * (vx - vz) * (vx - vz) + sq_term);
+  const real f15eq =
+      rho * (1.f / 36.f) *
+      (1 + 3 * (vy + vz) + 4.5f * (vy + vz) * (vy + vz) + sq_term);
+  const real f16eq =
+      rho * (1.f / 36.f) *
+      (1 - 3 * (vy + vz) + 4.5f * (vy + vz) * (vy + vz) + sq_term);
+  const real f17eq =
+      rho * (1.f / 36.f) *
+      (1 + 3 * (vy - vz) + 4.5f * (vy - vz) * (vy - vz) + sq_term);
+  const real f18eq =
+      rho * (1.f / 36.f) *
+      (1 - 3 * (vy - vz) + 4.5f * (vy - vz) * (vy - vz) + sq_term);
 
   // Compute the equilibrium temperature distribution
-  T0eq = T * (1.f / 7.f);
-  T1eq = T * (1.f / 7.f) * (1 + (7.f / 2.f) * vx);
-  T2eq = T * (1.f / 7.f) * (1 - (7.f / 2.f) * vx);
-  T3eq = T * (1.f / 7.f) * (1 + (7.f / 2.f) * vy);
-  T4eq = T * (1.f / 7.f) * (1 - (7.f / 2.f) * vy);
-  T5eq = T * (1.f / 7.f) * (1 + (7.f / 2.f) * vz);
-  T6eq = T * (1.f / 7.f) * (1 - (7.f / 2.f) * vz);
+  const real T0eq = T * (1.f / 7.f);
+  const real T1eq = T * (1.f / 7.f) * (1 + (7.f / 2.f) * vx);
+  const real T2eq = T * (1.f / 7.f) * (1 - (7.f / 2.f) * vx);
+  const real T3eq = T * (1.f / 7.f) * (1 + (7.f / 2.f) * vy);
+  const real T4eq = T * (1.f / 7.f) * (1 - (7.f / 2.f) * vy);
+  const real T5eq = T * (1.f / 7.f) * (1 + (7.f / 2.f) * vz);
+  const real T6eq = T * (1.f / 7.f) * (1 - (7.f / 2.f) * vz);
 
   // Difference to equilibrium
-  f1diff = f1 - f1eq;
-  f2diff = f2 - f2eq;
-  f3diff = f3 - f3eq;
-  f4diff = f4 - f4eq;
-  f5diff = f5 - f5eq;
-  f6diff = f6 - f6eq;
-  f7diff = f7 - f7eq;
-  f8diff = f8 - f8eq;
-  f9diff = f9 - f9eq;
-  f10diff = f10 - f10eq;
-  f11diff = f11 - f11eq;
-  f12diff = f12 - f12eq;
-  f13diff = f13 - f13eq;
-  f14diff = f14 - f14eq;
-  f15diff = f15 - f15eq;
-  f16diff = f16 - f16eq;
-  f17diff = f17 - f17eq;
-  f18diff = f18 - f18eq;
+  const real f1diff = f1 - f1eq;
+  const real f2diff = f2 - f2eq;
+  const real f3diff = f3 - f3eq;
+  const real f4diff = f4 - f4eq;
+  const real f5diff = f5 - f5eq;
+  const real f6diff = f6 - f6eq;
+  const real f7diff = f7 - f7eq;
+  const real f8diff = f8 - f8eq;
+  const real f9diff = f9 - f9eq;
+  const real f10diff = f10 - f10eq;
+  const real f11diff = f11 - f11eq;
+  const real f12diff = f12 - f12eq;
+  const real f13diff = f13 - f13eq;
+  const real f14diff = f14 - f14eq;
+  const real f15diff = f15 - f15eq;
+  const real f16diff = f16 - f16eq;
+  const real f17diff = f17 - f17eq;
+  const real f18diff = f18 - f18eq;
 
   // Non equilibrium stress-tensor for velocity
-  real Pi_x_x = f1diff + f2diff + f7diff + f8diff + f9diff + f10diff + f11diff +
-                f12diff + f13diff + f14diff;
-  real Pi_x_y = f7diff + f8diff - f9diff - f10diff;
-  real Pi_x_z = f11diff + f12diff - f13diff - f14diff;
-  real Pi_y_y = f3diff + f4diff + f7diff + f8diff + f9diff + f10diff + f15diff +
-                f16diff + f17diff + f18diff;
-  real Pi_y_z = f15diff + f16diff - f17diff - f18diff;
-  real Pi_z_z = f5diff + f6diff + f11diff + f12diff + f13diff + f14diff +
-                f15diff + f16diff + f17diff + f18diff;
+  const real Pi_x_x = f1diff + f2diff + f7diff + f8diff + f9diff + f10diff +
+                      f11diff + f12diff + f13diff + f14diff;
+  const real Pi_x_y = f7diff + f8diff - f9diff - f10diff;
+  const real Pi_x_z = f11diff + f12diff - f13diff - f14diff;
+  const real Pi_y_y = f3diff + f4diff + f7diff + f8diff + f9diff + f10diff +
+                      f15diff + f16diff + f17diff + f18diff;
+  const real Pi_y_z = f15diff + f16diff - f17diff - f18diff;
+  const real Pi_z_z = f5diff + f6diff + f11diff + f12diff + f13diff + f14diff +
+                      f15diff + f16diff + f17diff + f18diff;
   // Variance
-  real Q = Pi_x_x * Pi_x_x + 2 * Pi_x_y * Pi_x_y + 2 * Pi_x_z * Pi_x_z +
-           Pi_y_y * Pi_y_y + 2 * Pi_y_z * Pi_y_z + Pi_z_z * Pi_z_z;
+  const real Q = Pi_x_x * Pi_x_x + 2 * Pi_x_y * Pi_x_y + 2 * Pi_x_z * Pi_x_z +
+                 Pi_y_y * Pi_y_y + 2 * Pi_y_z * Pi_y_z + Pi_z_z * Pi_z_z;
   // Local stress tensor
-  real ST = (1 / (real)6) * (sqrt(nu * nu + 18 * C * C * sqrt(Q)) - nu);
+  const real ST = (1 / (real)6) * (sqrt(nu * nu + 18 * C * C * sqrt(Q)) - nu);
   // Modified relaxation time
-  real tau = 3 * (nu + ST) + (real)0.5;
+  const real tau = 3 * (nu + ST) + (real)0.5;
 
   dftmp3D(0, x, y, z, nx, ny, nz) = (1 - 1 / tau) * f0 + (1 / tau) * f0eq;
   dftmp3D(1, x, y, z, nx, ny, nz) = (1 - 1 / tau) * f1 + (1 / tau) * f1eq;
@@ -293,18 +309,19 @@ __device__ PhysicalQuantity compute(
   dftmp3D(18, x, y, z, nx, ny, nz) = (1 - 1 / tau) * f18 + (1 / tau) * f18eq;
 
   // Modified relaxation time for the temperature
-  tau = 3 * (nuT + ST / Pr_t) + (real)0.5;
+  const real tuaT = 3 * (nuT + ST / Pr_t) + (real)0.5;
 
   // Relax temperature
-  Tdftmp3D(0, x, y, z, nx, ny, nz) = (1 - 1 / tau) * T0 + (1 / tau) * T0eq;
-  Tdftmp3D(1, x, y, z, nx, ny, nz) = (1 - 1 / tau) * T1 + (1 / tau) * T1eq;
-  Tdftmp3D(2, x, y, z, nx, ny, nz) = (1 - 1 / tau) * T2 + (1 / tau) * T2eq;
-  Tdftmp3D(3, x, y, z, nx, ny, nz) = (1 - 1 / tau) * T3 + (1 / tau) * T3eq;
-  Tdftmp3D(4, x, y, z, nx, ny, nz) = (1 - 1 / tau) * T4 + (1 / tau) * T4eq;
-  Tdftmp3D(5, x, y, z, nx, ny, nz) = (1 - 1 / tau) * T5 + (1 / tau) * T5eq;
-  Tdftmp3D(6, x, y, z, nx, ny, nz) = (1 - 1 / tau) * T6 + (1 / tau) * T6eq;
+  Tdftmp3D(0, x, y, z, nx, ny, nz) = (1 - 1 / tuaT) * T0 + (1 / tuaT) * T0eq;
+  Tdftmp3D(1, x, y, z, nx, ny, nz) = (1 - 1 / tuaT) * T1 + (1 / tuaT) * T1eq;
+  Tdftmp3D(2, x, y, z, nx, ny, nz) = (1 - 1 / tuaT) * T2 + (1 / tuaT) * T2eq;
+  Tdftmp3D(3, x, y, z, nx, ny, nz) = (1 - 1 / tuaT) * T3 + (1 / tuaT) * T3eq;
+  Tdftmp3D(4, x, y, z, nx, ny, nz) = (1 - 1 / tuaT) * T4 + (1 / tuaT) * T4eq;
+  Tdftmp3D(5, x, y, z, nx, ny, nz) = (1 - 1 / tuaT) * T5 + (1 / tuaT) * T5eq;
+  Tdftmp3D(6, x, y, z, nx, ny, nz) = (1 - 1 / tuaT) * T6 + (1 / tuaT) * T6eq;
 
-  PhysicalQuantity phy = {.rho = rho, .T = T, .vx = vx, .vy = vy, .vz = vz};
+  const PhysicalQuantity phy = {
+      .rho = rho, .T = T, .vx = vx, .vy = vy, .vz = vz};
 
   // Average temperature and velocity
   averageDst[I4D(0, pos, size)] = averageSrc[I4D(0, pos, size)] + phy.T;
