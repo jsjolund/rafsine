428150ca6b0930280e134764eedef11e700618e1
jsjolund <j.sjolund@gmail.com>
2020-01-17 16:21:16 +0100

    Work on MRT thermal

--- python/lbm-double-mrt-les-boussinesq.py
+++ python/lbm-double-mrt-les-boussinesq.py
@@ -119,7 +119,7 @@ m1_13 = symbols('m1_13')
 m1_14 = symbols('m1_14')
 m1_15 = symbols('m1_15')
 S_bar = symbols('S_bar')
-nuE = symbols('nuE')
+ST = symbols('ST')
 
 rho = symbols('rho')
 en = symbols('en')
@@ -143,6 +143,8 @@ mz = symbols('mz')
 omega_e = symbols('omega_e')
 omega_xx = symbols('omega_xx')
 omega_ej = symbols('omega_ej')
+tau_V = symbols('tau_V')
+tau_T = symbols('tau_T')
 
 T = symbols('T')
 vx = symbols('vx')
@@ -152,7 +154,8 @@ vz = symbols('vz')
 Fup = symbols('Fup')
 Fdown = symbols('Fdown')
 
-src.define(mi_eq, mi_neq, omega, ni_eq,  ni_neq, omegaT, S_bar, nuE, T, Fup, Fdown,
+src.define(mi_eq, mi_neq, omega, ni_eq,  ni_neq, omegaT, S_bar, ST, 
+           T, Fup, Fdown, tau_V, tau_T,
            Matrix([Sxx, Syy, Szz, Sxy, Syz, Sxz]),
            Matrix([m1_1, m1_9, m1_11, m1_13, m1_14, m1_15]),
            Matrix([rho, en, epsilon, jx, qx, jy, qy, jz, qz,
@@ -260,28 +263,28 @@ M = Matrix([phi(ei.row(i)) for i in range(0, 19)]).transpose()
 m = M*fi
 src.comment('Macroscopic density')
 src.let(rho, m[0])
-src.comment('Transform velocity distribution functions to moment space')
-# Energy
+src.comment('Part of kinetic energy independent of density')
 src.let(en, m[1])
-# Energy square
+src.comment('Kinetic energy square')
 src.let(epsilon, m[2])
-# Momentum
+src.comment('Momentum')
 src.let(jx, m[3])
 src.let(jy, m[5])
 src.let(jz, m[7])
-# Energy flux
+src.comment('Energy flux independent of mass flux')
 src.let(qx, m[4])
 src.let(qy, m[6])
 src.let(qz, m[8])
-# Symmetric viscous stress tensor
+src.comment('Symmetric traceless viscous stress tensor')
 src.let(pxx3, m[9])
-src.let(pixx3, m[10])
 src.let(pww, m[11])
-src.let(piww, m[12])
 src.let(pxy, m[13])
 src.let(pyz, m[14])
 src.let(pxz, m[15])
-# Antisymmetric third-order moment
+src.comment('Fourth order moments')
+src.let(pixx3, m[10])
+src.let(piww, m[12])
+src.comment('Antisymmetric third-order moment')
 src.let(mx, m[16])
 src.let(my, m[17])
 src.let(mz, m[18])
@@ -289,18 +292,17 @@ src.let(mz, m[18])
 mi = Matrix([rho, en, epsilon, jx, qx, jy, qy, jz, qz, pxx3,
              pixx3, pww, piww, pxy, pyz, pxz, mx, my, mz])
 
-# Model stability constants
+src.comment('Model stability constants')
 src.let(omega_e, 0)
 src.let(omega_xx, 0)
 src.let(omega_ej, -475.0/63.0)
 
-# Macroscopic velocity
 src.comment('Macroscopic velocity')
 src.let(vx, jx/rho)
 src.let(vy, jy/rho)
 src.let(vz, jz/rho)
 
-# Velocity moment equilibirum PDFs
+src.comment('Velocity moment equilibirum distribution functions')
 m_eq0 = rho
 m_eq1 = -11.0*rho + 19.0/rho_0*(jx*jx + jy*jy + jz*jz)
 m_eq2 = omega_e*rho + omega_ej/rho_0*(jx*jx + jy*jy + jz*jz)
@@ -310,7 +312,7 @@ m_eq5 = jy
 m_eq6 = -2.0/3.0*jy
 m_eq7 = jz
 m_eq8 = -2.0/3.0*jz
-m_eq9 = 1.0/rho_0*(2.0*jx*jx - (jy*jy + jz*jz))
+m_eq9 = 1.0/rho_0*(2.0*jx*jx - jy*jy - jz*jz)
 m_eq10 = omega_xx*m_eq9
 m_eq11 = (jy*jy - jz*jz)/rho_0
 m_eq12 = omega_xx*m_eq11
@@ -322,11 +324,8 @@ m_eq17 = 0
 m_eq18 = 0
 m_eq = Matrix([m_eq0, m_eq1, m_eq2, m_eq3, m_eq4, m_eq5, m_eq6, m_eq7, m_eq8,
                m_eq9, m_eq10, m_eq11, m_eq12, m_eq13, m_eq14, m_eq15, m_eq16, m_eq17, m_eq18])
-
-src.comment('Velocity moment equilibirum distribution functions')
 src.let(mi_eq, m_eq)
 
-# LES strain rate tensor
 src.comment('LES strain rate tensor')
 src.let(m1_1, 38.0/3.0*(jx + jy + jz))
 src.let(m1_9, -2.0/3.0*(2.0*jx - jy - jz))
@@ -334,31 +333,23 @@ src.let(m1_11, -2.0/3.0*(jy - jz))
 src.let(m1_13, -1.0/3.0*(jx + jy))
 src.let(m1_14, -1.0/3.0*(jz + jy))
 src.let(m1_15, -1.0/3.0*(jx + jz))
-src.let(Sxx, 0.0 - m1_1/(38.0*rho_0) - m1_9/(2.0*rho_0))
-src.let(Syy, 0.0 - m1_1/(38.0*rho_0) + m1_9 /
-        (4.0*rho_0) - 3.0*m1_11/(4.0*rho_0))
-src.let(Szz, 0.0 - m1_1/(38.0*rho_0) + m1_9 /
-        (4.0*rho_0) + 3.0*m1_11/(4.0*rho_0))
-src.let(Sxy, -3.0*m1_13/(2.0*rho_0))
-src.let(Syz, -3.0*m1_14/(2.0*rho_0))
-src.let(Sxz, -3.0*m1_15/(2.0*rho_0))
+
+src.let(Sxx, -m1_1/(38.0*rho_0*dt) - m1_9/(2.0*rho_0*dt))
+src.let(Syy, -m1_1/(38.0*rho_0*dt) + m1_9 /
+        (4.0*rho_0*dt) - 3.0*m1_11/(4.0*rho_0*dt))
+src.let(Szz, -m1_1/(38.0*rho_0*dt) + m1_9 /
+        (4.0*rho_0*dt) + 3.0*m1_11/(4.0*rho_0*dt))
+src.let(Sxy, -3.0*m1_13/(2.0*rho_0*dt))
+src.let(Syz, -3.0*m1_14/(2.0*rho_0*dt))
+src.let(Sxz, -3.0*m1_15/(2.0*rho_0*dt))
+
+src.comment('Magnitude of strain rate tensor')
 src.let(S_bar, (2.0*(Sxx*Sxx + Syy*Syy + Szz*Szz +
                      Sxy*Sxy + Syz*Syz + Sxz*Sxz))**(1.0/2.0))
 
-# Eddy viscosity
-src.let(nuE, (C*dfw)**2.0*S_bar)
+src.comment('Filtered strain rate')
+src.let(ST, (C*dfw)**2.0*S_bar)
 
-
-# # Transformation matrix for transition from energy to moment space
-# def chi(ei):
-#     p0 = ei.norm()**0
-#     p1 = ei[0]
-#     p2 = ei[1]
-#     p3 = ei[2]
-#     p4 = 6.0 - 7.0*ei.norm()**2
-#     p5 = 3.0*ei[0]**2 - ei.norm()**2
-#     p6 = ei[1]**2 - ei[2]**2
-#     return [p0, p1, p2, p3, p4, p5, p6]
 
 # Transformation matrix for transition from energy to moment space
 def chi(ei):
@@ -366,14 +357,13 @@ def chi(ei):
     p1 = ei[0]
     p2 = ei[1]
     p3 = ei[2]
-    p4 = ei[0]**2 + ei[1]**2 + ei[2]**2
-    p5 = ei[0]**2 - ei[1]**2
-    p6 = ei[0]**2 - ei[2]**2
+    p4 = 6.0 - 7.0*ei.norm()**2
+    p5 = 3.0*ei[0]**2 - ei.norm()**2
+    p6 = ei[1]**2 - ei[2]**2
     return [p0, p1, p2, p3, p4, p5, p6]
 
-N = Matrix([chi(ei.row(i)) for i in range(0, 7)]).transpose()
 
-# pprint(N)
+N = Matrix([chi(ei.row(i)) for i in range(0, 7)]).transpose()
 
 # Transform temperature PDFs to moment space
 ni = N*Ti
@@ -386,7 +376,6 @@ n_eq1 = vx*T
 n_eq2 = vy*T
 n_eq3 = vz*T
 n_eq4 = 0.75*T
-# n_eq4 = (7.0*e_omegaT[0] - 1.0)*T
 n_eq5 = 0.0
 n_eq6 = 0.0
 n_eq = Matrix([n_eq0, n_eq1, n_eq2, n_eq3, n_eq4, n_eq5, n_eq6])
@@ -416,28 +405,35 @@ Fi[5] = Fup
 Fi[6] = Fdown
 
 # Collision matrix for temperature moments
-tau_xx = tau_yy = tau_zz = 1.0/((nuT + nuE/Pr_t)*6.0 + 0.5)
-tau_xy = 0
-tau_xz = 0
-tau_yz = 0
+src.comment('Modified heat diffusion')
+src.let(tau_T, 1.0/(5.0*(nuT + ST/Pr_t) + 0.5))
+tau_0 = 0.0
+tau_xx = tau_yy = tau_zz = tau_T
+tau_xy = 0.0
+tau_xz = 0.0
+tau_yz = 0.0
+tau_e = tau_v = 1.0  # /(3.0**(1.0/2.0)/3.0 - 0.5)
 
 Q_hat = Matrix([
-    [1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+    [tau_0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
     [0.0, tau_xx, tau_xy, tau_xz, 0.0, 0.0, 0.0],
     [0.0, tau_xy, tau_yy, tau_yz, 0.0, 0.0, 0.0],
     [0.0, tau_xz, tau_yz, tau_zz, 0.0, 0.0, 0.0],
-    [0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0],
-    [0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0],
-    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0],
+    [0.0, 0.0, 0.0, 0.0, tau_e, 0.0, 0.0],
+    [0.0, 0.0, 0.0, 0.0, 0.0, tau_v, 0.0],
+    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, tau_v],
 ])
 
 # Collision matrix for velocities in moment space
+src.comment('Modified shear viscosity')
+src.let(tau_V, 1.0/(3.0*(nu + ST) + 0.5))
+
 s1 = s4 = s6 = s8 = 0.0
 s2 = 1.19
 s3 = s11 = s13 = 1.4
 s5 = s7 = s9 = 1.2
 s17 = s18 = s19 = 1.98
-s10 = s12 = s14 = s15 = s16 = 2.0/(1.0 + 6.0*(nu + nuE))
+s10 = s12 = s14 = s15 = s16 = tau_V
 S_hat = sympy.diag(s1, s2, s3, s4, s5, s6, s7, s8, s9, s10,
                    s11, s12, s13, s14, s15, s16, s17, s18, s19)
 
