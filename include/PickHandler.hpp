ec9fd182f1ea13c6d39245be80a0864eaa23cd71
Ubuntu <ubuntu@p01r03srv19.maas>
2020-01-28 15:29:10 +0100

    Comment out unused gui mouse pick handler

--- include/PickHandler.hpp
+++ include/PickHandler.hpp
@@ -1,67 +1,67 @@
-#pragma once
+// #pragma once
 
-#include <osgViewer/Viewer>
+// #include <osgViewer/Viewer>
 
-#include <sstream>
+// #include <sstream>
 
-#include "CFDScene.hpp"
+// #include "CFDScene.hpp"
 
-/**
- * @brief Class to handle mouse click events on the 3D voxel visualization
- *
- */
-class PickHandler : public osgGA::GUIEventHandler {
- private:
-  CFDScene* m_scene;
+// /**
+//  * @brief Class to handle mouse click events on the 3D voxel visualization
+//  *
+//  */
+// class PickHandler : public osgGA::GUIEventHandler {
+//  private:
+//   CFDScene* m_scene;
 
- protected:
-  ~PickHandler() {}
+//  protected:
+//   ~PickHandler() {}
 
- public:
-  /**
-   * @brief Construct a new Pick Handler
-   *
-   * @param scene The CFD scene to pick objects in
-   */
-  explicit PickHandler(CFDScene* scene);
-  /**
-   * @brief Executed when user (double) clicks on the voxel model
-   *
-   * @param view The view
-   * @param ea Adapter for the click event
-   * @return true A valid target was picked
-   * @return false No valid target was picked
-   */
-  bool pick(osgViewer::View* view, const osgGA::GUIEventAdapter& ea);
-  /**
-   * @brief Filters input events and calls the picking function
-   *
-   * @param ea  Adapter for the click event
-   * @param aa Adapter containing the view
-   * @return true The event was handled
-   * @return false The event was not handled
-   */
-  bool handle(const osgGA::GUIEventAdapter& ea,
-              osgGA::GUIActionAdapter& aa,
-              osg::Object*,
-              osg::NodeVisitor*) override;
-  /**
-   * @brief Alternative input event handle
-   *
-   * @param event
-   * @param object
-   * @param nv
-   * @return true
-   * @return false
-   */
-  bool handle(osgGA::Event* event,
-              osg::Object* object,
-              osg::NodeVisitor* nv) override;
-  /**
-   * @brief Alternative input event handle
-   *
-   * @return true
-   * @return false
-   */
-  bool handle(const osgGA::GUIEventAdapter&, osgGA::GUIActionAdapter&) override;
-};
+//  public:
+//   /**
+//    * @brief Construct a new Pick Handler
+//    *
+//    * @param scene The CFD scene to pick objects in
+//    */
+//   explicit PickHandler(CFDScene* scene);
+//   /**
+//    * @brief Executed when user (double) clicks on the voxel model
+//    *
+//    * @param view The view
+//    * @param ea Adapter for the click event
+//    * @return true A valid target was picked
+//    * @return false No valid target was picked
+//    */
+//   bool pick(osgViewer::View* view, const osgGA::GUIEventAdapter& ea);
+//   /**
+//    * @brief Filters input events and calls the picking function
+//    *
+//    * @param ea  Adapter for the click event
+//    * @param aa Adapter containing the view
+//    * @return true The event was handled
+//    * @return false The event was not handled
+//    */
+//   bool handle(const osgGA::GUIEventAdapter& ea,
+//               osgGA::GUIActionAdapter& aa,
+//               osg::Object*,
+//               osg::NodeVisitor*) override;
+//   /**
+//    * @brief Alternative input event handle
+//    *
+//    * @param event
+//    * @param object
+//    * @param nv
+//    * @return true
+//    * @return false
+//    */
+//   bool handle(osgGA::Event* event,
+//               osg::Object* object,
+//               osg::NodeVisitor* nv) override;
+//   /**
+//    * @brief Alternative input event handle
+//    *
+//    * @return true
+//    * @return false
+//    */
+//   bool handle(const osgGA::GUIEventAdapter&, osgGA::GUIActionAdapter&) override;
+// };
--- src/gui/PickHandler.cpp
+++ src/gui/PickHandler.cpp
@@ -1,56 +1,56 @@
-#include "PickHandler.hpp"
+// #include "PickHandler.hpp"
 
-PickHandler::PickHandler(CFDScene* scene)
-    : osgGA::GUIEventHandler(), m_scene(scene) {}
+// PickHandler::PickHandler(CFDScene* scene)
+//     : osgGA::GUIEventHandler(), m_scene(scene) {}
 
-bool PickHandler::handle(const osgGA::GUIEventAdapter& ea,
-                         osgGA::GUIActionAdapter& aa,
-                         osg::Object*,
-                         osg::NodeVisitor*) {
-  if (ea.getButton() != osgGA::GUIEventAdapter::LEFT_MOUSE_BUTTON) return false;
+// bool PickHandler::handle(const osgGA::GUIEventAdapter& ea,
+//                          osgGA::GUIActionAdapter& aa,
+//                          osg::Object*,
+//                          osg::NodeVisitor*) {
+//   if (ea.getButton() != osgGA::GUIEventAdapter::LEFT_MOUSE_BUTTON) return false;
 
-  switch (ea.getEventType()) {
-    case (osgGA::GUIEventAdapter::DOUBLECLICK): {
-      osgViewer::View* view = dynamic_cast<osgViewer::View*>(&aa);
-      if (view) return pick(view, ea);
-      return false;
-    }
-    default:
-      return false;
-  }
-}
+//   switch (ea.getEventType()) {
+//     case (osgGA::GUIEventAdapter::DOUBLECLICK): {
+//       osgViewer::View* view = dynamic_cast<osgViewer::View*>(&aa);
+//       if (view) return pick(view, ea);
+//       return false;
+//     }
+//     default:
+//       return false;
+//   }
+// }
 
-bool PickHandler::handle(osgGA::Event* event,
-                         osg::Object* object,
-                         osg::NodeVisitor* nv) {
-  return handle(*(event->asGUIEventAdapter()),
-                *(nv->asEventVisitor()->getActionAdapter()), object, nv);
-}
+// bool PickHandler::handle(osgGA::Event* event,
+//                          osg::Object* object,
+//                          osg::NodeVisitor* nv) {
+//   return handle(*(event->asGUIEventAdapter()),
+//                 *(nv->asEventVisitor()->getActionAdapter()), object, nv);
+// }
 
-bool PickHandler::handle(const osgGA::GUIEventAdapter& ea,
-                         osgGA::GUIActionAdapter& aa) {
-  return handle(ea, aa, NULL, NULL);
-}
+// bool PickHandler::handle(const osgGA::GUIEventAdapter& ea,
+//                          osgGA::GUIActionAdapter& aa) {
+//   return handle(ea, aa, NULL, NULL);
+// }
 
-bool PickHandler::pick(osgViewer::View* view,
-                       const osgGA::GUIEventAdapter& ea) {
-  osgUtil::LineSegmentIntersector::Intersections intersections;
-  // Hide axis model to prevent clicking on it
-  m_scene->setAxesVisible(false);
-  // Find intersections from mouse click
-  if (view->computeIntersections(ea, intersections)) {
-    for (osgUtil::LineSegmentIntersector::Intersections::iterator hitr =
-             intersections.begin();
-         hitr != intersections.end(); ++hitr) {
-      if (hitr->drawable.valid()) {
-        if (m_scene->selectVoxel(hitr->getWorldIntersectPoint())) {
-          m_scene->setAxesVisible(true);
-          return true;
-        }
-      }
-    }
-  }
-  // Show the axis model again
-  m_scene->setAxesVisible(true);
-  return false;
-}
+// bool PickHandler::pick(osgViewer::View* view,
+//                        const osgGA::GUIEventAdapter& ea) {
+//   osgUtil::LineSegmentIntersector::Intersections intersections;
+//   // Hide axis model to prevent clicking on it
+//   m_scene->setAxesVisible(false);
+//   // Find intersections from mouse click
+//   if (view->computeIntersections(ea, intersections)) {
+//     for (osgUtil::LineSegmentIntersector::Intersections::iterator hitr =
+//              intersections.begin();
+//          hitr != intersections.end(); ++hitr) {
+//       if (hitr->drawable.valid()) {
+//         if (m_scene->selectVoxel(hitr->getWorldIntersectPoint())) {
+//           m_scene->setAxesVisible(true);
+//           return true;
+//         }
+//       }
+//     }
+//   }
+//   // Show the axis model again
+//   m_scene->setAxesVisible(true);
+//   return false;
+// }
