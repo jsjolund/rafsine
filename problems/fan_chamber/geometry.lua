c37b69ebbd34b6799aab7444e4376eb5b5a8cdd2
Ubuntu <ubuntu@p01r03srv19.maas>
2020-01-28 15:26:07 +0100

    Add fan chamber problem

---
+++ problems/fan_chamber/geometry.lua
@@ -0,0 +1,130 @@
+package.path = package.path .. "./?.lua;lua/?.lua"
+require "problems/fan_chamber/settings"
+require "VoxelGeometry"
+
+print("Time-step : " .. uc:N_to_s(1) .. " s")
+print("Creating geometry of size ", nx, ny, nz)
+vox = VoxelGeometry(nx, ny, nz)
+
+-- Length of a voxel in meters
+C_L = uc:C_L()
+print("C_L = "..C_L.." m")
+
+-- Set domain boundary conditions
+vox:addWallXmin()
+vox:addWallXmax()
+vox:addWallYmin()
+vox:addWallYmax()
+vox:addWallZmin()
+vox:addWallZmax()
+
+ventFlow = 5
+fanFlow = 5
+boxSize = 2.5
+
+fanSpeed = uc:Q_to_Ulu(fanFlow, (boxSize-C_L*2)*(boxSize-C_L*2))
+ventSpeed = uc:Q_to_Ulu(ventFlow, (mx-C_L*2)*(mz-C_L*2))
+
+vox:addQuadBC(
+  {
+    origin = {C_L, 0, C_L},
+    dir1 = {mx-C_L*2, 0, 0},
+    dir2 = {0, 0, mz-C_L*2},
+    typeBC = "inlet",
+    normal = {0, 1, 0},
+    velocity = {0, ventSpeed, 0},
+    temperature = {
+      type_ = "constant", 
+      value = 20
+    },
+    mode = "overwrite",
+    name = "vent_input",
+  })
+
+vox:addQuadBC(
+  {
+    origin = {C_L, my, C_L},
+    dir1 = {mx-C_L*2, 0, 0},
+    dir2 = {0, 0, mz-C_L*2},
+    typeBC = "inlet",
+    normal = {0, -1, 0},
+    velocity = {0, ventSpeed, 0},
+    temperature = {type_ = "zeroGradient"},
+    mode = "overwrite",
+    name = "vent_output",
+  })
+
+vox:addSensor(
+  {
+    min = {C_L, C_L, C_L},
+    max = {mx-C_L, C_L, mz-C_L},
+    name = "vent_input_sensor"
+  })
+
+vox:addSensor(
+  {
+    min = {C_L, my-C_L, C_L},
+    max = {mx-C_L, my-C_L, mz-C_L},
+    name = "vent_output_sensor"
+  })
+
+fanMin = {
+  mx/2 - boxSize/2,
+  my/2 - boxSize/2,
+  mz/2 - boxSize/2,
+}
+fanMax = {
+  mx/2 + boxSize/2,
+  my/2 + boxSize/2,
+  mz/2 + boxSize/2,
+}
+vox:addSolidBox(
+  {
+    name = "fan",
+    min = fanMin,
+    max = fanMax,
+  })
+
+vox:addQuadBC(
+  {
+    origin = vector(fanMin) + vector({C_L, 0.0, C_L}),
+    dir1 = {boxSize-2*C_L, 0.0, 0.0},
+    dir2 = {0.0, 0.0, boxSize-2*C_L},
+    typeBC = "inlet",
+    normal = {0, -1, 0},
+    velocity = {0, fanSpeed, 0},
+    temperature = {type_ = "zeroGradient"},
+    mode = "overwrite",
+    name = "fan_input",
+  })
+
+vox:addQuadBC(
+  {
+    origin = vector({fanMin[1], fanMax[2], fanMin[3]}) + vector({C_L, 0.0, C_L}),
+    dir1 = {boxSize-2*C_L, 0.0, 0.0},
+    dir2 = {0.0, 0.0, boxSize-2*C_L},
+    typeBC = "inlet",
+    normal = {0, 1, 0},
+    velocity = {0, fanSpeed, 0},
+    temperature = {
+      type_ = "relative",
+      value = 20,
+      rel_pos = boxSize,
+    },
+    mode = "overwrite",
+    name = "fan_output",
+  })
+
+vox:addSensor(
+  {
+    min = vector(fanMin) + vector({C_L, -C_L, C_L}),
+    max = vector(fanMin) + vector({boxSize-C_L, -C_L, boxSize-C_L}),
+    name = "fan_input_sensor"
+  })
+
+vox:addSensor(
+  {
+    min = vector({fanMin[1], fanMax[2], fanMin[3]}) + vector({C_L, C_L, C_L}),
+    max = vector({fanMin[1], fanMax[2], fanMin[3]}) + vector({boxSize-C_L, C_L, boxSize-C_L}),
+    name = "fan_output_sensor"
+  })
---
+++ problems/fan_chamber/settings.lua
@@ -0,0 +1,59 @@
+package.path = package.path .. ";lua/?.lua"
+require "UnitConverter"
+
+-- Physical Settings --
+uc = UnitConverter(
+  {
+    reference_length_in_meters = 5.0,
+    -- reference_length_in_number_of_nodes = 32,
+    reference_length_in_number_of_nodes = 64,
+    -- reference_length_in_number_of_nodes = 96,
+    -- reference_length_in_number_of_nodes = 128,
+    -- reference_length_in_number_of_nodes = 192,
+    -- reference_length_in_number_of_nodes = 256,
+    -- reference_length_in_number_of_nodes = 512,
+    reference_speed_in_meters_per_second = 1.0,
+    reference_speed_in_lattice_units = 0.1,
+    temperature_conversion_factor = 1,
+    reference_temperature_physical = 0,
+    reference_temperature_LBM = 0
+  }
+)
+
+-- Velocity conversion factor
+C_U = uc.C_U
+C_L = uc.C_L
+
+-- Size in meters
+mx = 5.0
+my = 20.0
+mz = 5.0
+
+-- Size of the lattice
+nx = uc:m_to_lu(mx) + 1
+ny = uc:m_to_lu(my) + 1
+nz = uc:m_to_lu(mz) + 1
+
+-- Smagorinsky constant
+-- C = 0.02 --BGK
+C = 0.1
+
+-- Kinematic viscosity of air
+nu = uc:Nu_to_lu(1.506e-5)
+-- Thermal diffusivity
+nuT = uc:Nu_to_lu(21.70e-6)
+
+-- Turbulent Prandtl number
+Pr_t = 0.9
+
+-- Gravity * thermal expansion
+--gBetta = uc:gBetta_to_lu(3.0e-3)
+gBetta = 0
+
+-- Initial temperature
+Tinit = uc:Temp_to_lu(20)
+-- Reference temperature
+Tref = Tinit
+
+-- Averaging period in seconds
+avgPeriod = 10
