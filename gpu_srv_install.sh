f705e50cf8a3f436fb7d668f503516ae2c99be87
Ubuntu <ubuntu@p01r03srv19.maas>
2020-03-27 14:55:00 +0100

    Update install doc

--- gpu_srv_install.sh
+++ gpu_srv_install.sh
@@ -4,14 +4,21 @@ sudo apt-get update
 
 sudo apt-get upgrade
 
-sudo apt-get -y install ccache clang-format libluajit-5.1-common libluajit-5.1-dev liblua5.1-0 liblua5.1-0-dev luarocks libboost-all-dev nvidia-driver-390 nvidia-utils-390 nvidia-cuda-dev nvidia-cuda-gdb nvidia-cuda-toolkit mesa-utils qt5-default qtbase5-dev xubuntu-desktop geany geany-plugins lightdm lightdm-gtk-greeter xorg cmake clang libgif-dev librsvg2-dev libxine2-dev libpth-dev gdb zsh zsh-syntax-highlighting doxygen graphviz dia mscgen libglm-dev python-pip freeglut3-dev libjpeg9-dev libsdl-dev libsdl2-dev libgstreamer1.0-dev libxml2-dev libcurl4-gnutls-dev libpoppler-cpp-dev libpoppler-glib-dev
+# Build dependencies
+sudo apt-get -y install libluajit-5.1-common libluajit-5.1-dev liblua5.1-0 liblua5.1-0-dev luarocks libboost-all-dev nvidia-cuda-dev nvidia-cuda-toolkit qt5-default qtbase5-dev cmake libglm-dev
+sudo luarocks install multikey
+sudo luarocks install penlight
+
+# Dev dependencies
+sudo apt-get -y install ccache clang clang-format gdb doxygen graphviz dia mscgen
+pip3 install cpplint cmake_format numpy sympy pandas matplotlib scipy
+
+# Runtime dependencies and other tools
+sudo apt-get -y install nvidia-driver-390 nvidia-utils-390 nvidia-cuda-gdb mesa-utils xubuntu-desktop geany geany-plugins lightdm lightdm-gtk-greeter xorg libgif-dev librsvg2-dev libxine2-dev libpth-dev zsh zsh-syntax-highlighting python3-pip freeglut3-dev libjpeg9-dev libsdl-dev libsdl2-dev libgstreamer1.0-dev libxml2-dev libcurl4-gnutls-dev libpoppler-cpp-dev libpoppler-glib-dev
 
 # VirtualGL
 wget -O virtualgl.deb https://downloads.sourceforge.net/project/virtualgl/2.6.3/virtualgl_2.6.3_amd64.deb
 sudo dpkg -i virtualgl.deb
-echo "export PATH=\$PATH:/opt/TurboVNC/bin:/opt/VirtualGL/bin:~/.local/bin" >> ~/.bashrc
-echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/usr/lib64" >> ~/.bashdc
-source ~/.bashrc
 
 # TurboVNC
 wget -O turbovnc.deb https://sourceforge.net/projects/turbovnc/files/2.2.3/turbovnc_2.2.3_amd64.deb/download
@@ -20,7 +27,8 @@ sudo dpkg -i turbovnc.deb
 # VScode
 wget -O vscode.deb https://packages.microsoft.com/repos/vscode/pool/main/c/code/code_1.41.1-1576681836_amd64.deb
 sudo dpkg -i vscode.deb
-pip install cpplint cmake_format numpy sympy pandas matplotlib scipy
+code --install-extension njpwerner.autodocstring
+code --install-extension davidanson.vscode-markdownlint
 code --install-extension austin.code-gnu-global
 code --install-extension cheshirekow.cmake-format
 code --install-extension cschlosser.doxdocgen
@@ -36,7 +44,6 @@ code --install-extension slevesque.vscode-hexdump
 code --install-extension twxs.cmake
 code --install-extension webfreak.debug
 code --install-extension xaver.clang-format
-
 wget -O ~/.config/Code/User/keybindings.json https://gist.githubusercontent.com/jsjolund/fd45ea95b31b35dc9f4d6857b4f97cd4/raw/a0ad291188225d8e6f27e4cbd49456da9b2f679f/keybindings.json
 
 # Disable system sleep
@@ -44,8 +51,8 @@ sudo apt-get -y remove light-locker xscreensaver
 sudo apt autoremove
 sudo systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
 
-sudo luarocks install multikey
-sudo luarocks install penlight
+# Set time zone to Europe/Stockholm
+sudo ln -fs /usr/share/zoneinfo/Europe/Stockholm /etc/localtime && sudo dpkg-reconfigure --frontend noninteractive tzdata
 
 # Configure VirtualGL
 wget https://gist.githubusercontent.com/jsjolund/c783b011e2ea2abee6a8c91de056f3c5/raw/9ce1b031a2b5d5ec7db05da48182535a0f46b144/xorg.conf
@@ -56,29 +63,23 @@ sudo vglserver_config -config -s -f -t
 sudo usermod -aG vglusers ubuntu
 sudo modprobe nvidia_drm nvidia_modeset nvidia_uvm nvidia
 
+# Disable nouveau drivers
 sudo bash -c "echo blacklist nouveau > /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
 sudo bash -c "echo options nouveau modeset=0 >> /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
 sudo update-initramfs -u
 
 sudo systemctl set-default graphical.target
 
-mkdir ~/code
-
 # Install OpenSceneGraph
-cd ~/code
+cd ~
 git clone https://github.com/openscenegraph/OpenSceneGraph.git
 cd OpenSceneGraph
 cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release
 make -j$(nproc)
 sudo make install
 
-# Rafsine
-cd ~/code
-git clone https://github.com/jsjolund/rafsine.git
-cd rafsine
-git config credential.helper store
-
 #Zsh
+cd ~
 zsh
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 setopt EXTENDED_GLOB
@@ -89,11 +90,17 @@ sudo chsh -s /bin/zsh ubuntu
 echo "export PATH=\$PATH:/opt/TurboVNC/bin:/opt/VirtualGL/bin:~/.local/bin" >> ~/.zpreztorc
 echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/usr/lib64" >> ~/.zpreztorc
 source ~/.zpreztorc
-
-# Set time zone to Europe/Stockholm
-sudo ln -fs /usr/share/zoneinfo/Europe/Stockholm /etc/localtime && sudo dpkg-reconfigure --frontend noninteractive tzdata
+echo "export PATH=\$PATH:/opt/TurboVNC/bin:/opt/VirtualGL/bin:~/.local/bin" >> ~/.bashrc
+echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/usr/lib64" >> ~/.bashrc
+source ~/.bashrc
 
 sudo reboot
+
+# Rafsine
+# cd ~
+# git clone https://github.com/jsjolund/rafsine.git
+# cd rafsine
+# git config credential.helper store
 
 # Start the vnc
 # ssh -x -e none -L 5903:127.0.0.1:5901 -p 31759 ubuntu@109.225.89.161 -i ~/.ssh/gpusrv_rsa
