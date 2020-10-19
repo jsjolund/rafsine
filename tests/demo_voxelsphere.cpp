dceb864e5a59b5e5b363591689c991b62a1d82ae
Johannes Sjölund <johannes.sjolund@ri.se>
2020-10-19 09:44:04 +0200

    Work on sphere

--- tests/demo_voxelsphere.cpp
+++ tests/demo_voxelsphere.cpp
@@ -1,17 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
 #include <osg/ArgumentParser>
 #include <osg/Shape>
 #include <osg/ShapeDrawable>
 #include <osg/Vec3>
 #include <osgViewer/Viewer>
 #include <osgViewer/ViewerEventHandlers>
-
-#include <stdio.h>
-#include <unistd.h>
-#include <algorithm>
-#include <cmath>
-#include <iostream>
 #include <vector>
 
+#include "DdQq.hpp"
+#include "InputEventHandler.hpp"
 #include "Vector3.hpp"
 
 class VoxelSphere {
@@ -33,7 +35,9 @@ class VoxelSphere {
     for (;;) {
       if ((z = -z) >= 0) {
         if ((y = -y) >= 0) {
-          if ((x = -x) >= 0) { break; }
+          if ((x = -x) >= 0) {
+            break;
+          }
         }
       }
       fill(x, y, z);
@@ -42,24 +46,35 @@ class VoxelSphere {
 
   void fillAll(int x, int y, int z) {
     fillSigns(x, y, z);
-    if (z > y) { fillSigns(x, z, y); }
-    if (z > x && z > y) { fillSigns(z, y, x); }
+    if (z > y) {
+      fillSigns(x, z, y);
+    }
+    if (z > x && z > y) {
+      fillSigns(z, y, x);
+    }
   }
 
  protected:
   bool get(unsigned int x, unsigned int y, unsigned int z) {
     try {
       return m_grid.at(idx(x, y, z));
-    } catch (const std::exception& e) { return false; }
+    } catch (const std::exception& e) {
+      return false;
+    }
   }
+
   vector3<int> getNormal(unsigned int x, unsigned int y, unsigned int z) {
     return m_normals.at(idx(x, y, z));
   }
+
   bool getFromOrigin(int x, int y, int z) {
     try {
       return m_grid.at(idxn(x, y, z));
-    } catch (const std::exception& e) { return false; }
+    } catch (const std::exception& e) {
+      return false;
+    }
   }
+
   unsigned int getSizeX() { return m_n; }
   unsigned int getSizeY() { return m_n; }
   unsigned int getSizeZ() { return m_n; }
@@ -85,7 +100,7 @@ class VoxelSphere {
       }
     }
     std::vector<bool> cornerGrid(m_n * m_n * m_n);
-    const int r = floor(R) + 1;
+    const int r = floor(R);
     for (int x = -r; x < 0; x++)
       for (int y = -r; y < r; y++)
         for (int z = -r; z < r; z++) {
@@ -141,54 +156,48 @@ class VoxelSphere {
             cornerGrid.at(idxn(x, y, z - 1)) = true;
         }
 
-    // for (unsigned int x = 0; x < m_n; x++)
-    //   for (unsigned int y = 0; y < m_n; y++)
-    //     for (unsigned int z = 0; z < m_n; z++) {
-    //       if (cornerGrid.at(idx(x, y, z))) m_grid.at(idx(x, y, z)) = true;
-    //     }
+    for (unsigned int x = 0; x < m_n; x++)
+      for (unsigned int y = 0; y < m_n; y++)
+        for (unsigned int z = 0; z < m_n; z++) {
+          if (cornerGrid.at(idx(x, y, z))) m_grid.at(idx(x, y, z)) = true;
+        }
 
-    // for (int x = -r; x <= 0; x++)
-    //   for (int y = -r; y < r; y++)
-    //     for (int z = -r; z < r; z++) {
-    //       if (getFromOrigin(x, y, z))
-    //         m_normals.at(idxn(x, y, z)) =
-    //             m_normals.at(idxn(x, y, z)) + vector3<int>(-1, 0, 0);
-    //     }
-    // for (int x = 0; x <= r; x++)
-    //   for (int y = -r; y < r; y++)
-    //     for (int z = -r; z < r; z++) {
-    //       if (getFromOrigin(x, y, z))
-    //         m_normals.at(idxn(x, y, z)) =
-    //             m_normals.at(idxn(x, y, z)) + vector3<int>(1, 0, 0);
-    //     }
-    // for (int x = -r; x < r; x++)
-    //   for (int y = -r; y < 0; y++)
-    //     for (int z = -r; z < r; z++) {
-    //       if (getFromOrigin(x, y, z))
-    //         m_normals.at(idxn(x, y, z)) =
-    //             m_normals.at(idxn(x, y, z)) + vector3<int>(0, -1, 0);
-    //     }
-    // for (int x = -r; x < r; x++)
-    //   for (int y = 0; y <= r; y++)
-    //     for (int z = -r; z < r; z++) {
-    //       if (getFromOrigin(x, y, z))
-    //         m_normals.at(idxn(x, y, z)) =
-    //             m_normals.at(idxn(x, y, z)) + vector3<int>(0, 1, 0);
-    //     }
-    // for (int x = -r; x < r; x++)
-    //   for (int y = -r; y < r; y++)
-    //     for (int z = -r; z < 0; z++) {
-    //       if (getFromOrigin(x, y, z))
-    //         m_normals.at(idxn(x, y, z)) =
-    //             m_normals.at(idxn(x, y, z)) + vector3<int>(0, 0, -1);
-    //     }
-    // for (int x = -r; x < r; x++)
-    //   for (int y = -r; y < r; y++)
-    //     for (int z = 0; z <= r; z++) {
-    //       if (getFromOrigin(x, y, z))
-    //         m_normals.at(idxn(x, y, z)) =
-    //             m_normals.at(idxn(x, y, z)) + vector3<int>(0, 0, 1);
-    //     }
+    for (int x = -r; x <= 0; x++)
+      for (int y = -r; y < r; y++)
+        for (int z = -r; z < r; z++) {
+          if (getFromOrigin(x, y, z))
+            m_normals.at(idxn(x, y, z)) += vector3<int>(-1, 0, 0);
+        }
+    for (int x = 0; x <= r; x++)
+      for (int y = -r; y < r; y++)
+        for (int z = -r; z < r; z++) {
+          if (getFromOrigin(x, y, z))
+            m_normals.at(idxn(x, y, z)) += vector3<int>(1, 0, 0);
+        }
+    for (int x = -r; x < r; x++)
+      for (int y = -r; y < 0; y++)
+        for (int z = -r; z < r; z++) {
+          if (getFromOrigin(x, y, z))
+            m_normals.at(idxn(x, y, z)) += vector3<int>(0, -1, 0);
+        }
+    for (int x = -r; x < r; x++)
+      for (int y = 0; y <= r; y++)
+        for (int z = -r; z < r; z++) {
+          if (getFromOrigin(x, y, z))
+            m_normals.at(idxn(x, y, z)) += vector3<int>(0, 1, 0);
+        }
+    for (int x = -r; x < r; x++)
+      for (int y = -r; y < r; y++)
+        for (int z = -r; z < 0; z++) {
+          if (getFromOrigin(x, y, z))
+            m_normals.at(idxn(x, y, z)) += vector3<int>(0, 0, -1);
+        }
+    for (int x = -r; x < r; x++)
+      for (int y = -r; y < r; y++)
+        for (int z = 0; z <= r; z++) {
+          if (getFromOrigin(x, y, z))
+            m_normals.at(idxn(x, y, z)) += vector3<int>(0, 0, 1);
+        }
   }
 };
 
@@ -200,13 +209,53 @@ class OSGVoxelSphere : VoxelSphere {
   const osg::Vec4 m_gray;
 
  public:
+  std::vector<osg::ref_ptr<osg::Node>>* m_arrows;
+  std::vector<osg::ref_ptr<osg::Node>>* m_boxes;
   osg::ref_ptr<osg::Group> m_root;
 
-  void add(osg::ref_ptr<osg::Box> box, osg::Vec4 color) {
+  osg::ref_ptr<osg::Node> add(osg::Vec4 color, float x, float y, float z,
+                              float nx = 1, float ny = 1, float nz = 1) {
+    osg::ref_ptr<osg::Box> box =
+        new osg::Box(osg::Vec3(x, y, z) + osg::Vec3(10, 10, 10), nx, ny, nz);
     osg::ref_ptr<osg::ShapeDrawable> sd = new osg::ShapeDrawable();
     sd->setColor(color);
     sd->setShape(box);
     m_root->addChild(sd);
+    return sd;
+  }
+
+  void addArrow(float x, float y, float z, D3Q7::Enum dir) {
+    osg::ref_ptr<osg::Node> n;
+    float o = 0.5;
+    float l = 0.75;
+    float w = 0.05;
+    switch (dir) {
+      case D3Q7::Enum::X_AXIS_POS:
+        n = add(m_red, x + o, y, z, l, w, w);
+        break;
+      case D3Q7::Enum::X_AXIS_NEG:
+        n = add(m_red, x - o, y, z, l, w, w);
+        break;
+      case D3Q7::Enum::Y_AXIS_POS:
+        n = add(m_green, x, y + o, z, w, l, w);
+        break;
+      case D3Q7::Enum::Y_AXIS_NEG:
+        n = add(m_green, x, y - o, z, w, l, w);
+        break;
+      case D3Q7::Enum::Z_AXIS_POS:
+        n = add(m_blue, x, y, z + o, w, w, l);
+        break;
+      case D3Q7::Enum::Z_AXIS_NEG:
+        n = add(m_blue, x, y, z - o, w, w, l);
+        break;
+      default:
+        break;
+    }
+    m_arrows->push_back(n);
+  }
+
+  void addBox(float x, float y, float z) {
+    m_boxes->push_back(add(m_gray, x, y, z));
   }
 
   explicit OSGVoxelSphere(float R)
@@ -215,6 +264,8 @@ class OSGVoxelSphere : VoxelSphere {
         m_green(0, 1, 0, 1),
         m_blue(0, 0, 1, 1),
         m_gray(0.5, 0.5, 0.5, 0.5),
+        m_arrows(new std::vector<osg::ref_ptr<osg::Node>>()),
+        m_boxes(new std::vector<osg::ref_ptr<osg::Node>>()),
         m_root(new osg::Group) {
     for (unsigned int x = 0; x < getSizeX(); x++)
       for (unsigned int y = 0; y < getSizeY(); y++)
@@ -222,32 +273,53 @@ class OSGVoxelSphere : VoxelSphere {
           if (get(x, y, z)) {
             vector3<int> normal = getNormal(x, y, z);
             if (normal.x() > 0) {
-              add(new osg::Box(osg::Vec3f(x + 0.25, y, z), 0.75, 0.1, 0.1),
-                  m_red);
-            }
-            if (normal.y() > 0) {
-              add(new osg::Box(osg::Vec3f(x, y + 0.25, z), 0.1, 0.75, 0.1),
-                  m_green);
-            }
-            if (normal.z() > 0) {
-              add(new osg::Box(osg::Vec3f(x, y, z + 0.25), 0.1, 0.1, 0.75),
-                  m_blue);
+              addArrow(x, y, z, D3Q7::Enum::X_AXIS_POS);
             }
             if (normal.x() < 0) {
-              add(new osg::Box(osg::Vec3f(x - 0.25, y, z), 0.75, 0.1, 0.1),
-                  m_red);
+              addArrow(x, y, z, D3Q7::Enum::X_AXIS_NEG);
+            }
+            if (normal.y() > 0) {
+              addArrow(x, y, z, D3Q7::Enum::Y_AXIS_POS);
             }
             if (normal.y() < 0) {
-              add(new osg::Box(osg::Vec3f(x, y - 0.25, z), 0.1, 0.75, 0.1),
-                  m_green);
+              addArrow(x, y, z, D3Q7::Enum::Y_AXIS_NEG);
+            }
+            if (normal.z() > 0) {
+              addArrow(x, y, z, D3Q7::Enum::Z_AXIS_POS);
             }
             if (normal.z() < 0) {
-              add(new osg::Box(osg::Vec3f(x, y, z - 0.25), 0.1, 0.1, 0.75),
-                  m_blue);
+              addArrow(x, y, z, D3Q7::Enum::Z_AXIS_NEG);
             }
-            add(new osg::Box(osg::Vec3f(x, y, z), 1), m_gray);
+            addBox(x, y, z);
           }
         }
+  }
+};
+
+class MyKeyboardHandler : public InputEventHandler {
+ private:
+  std::vector<osg::ref_ptr<osg::Node>>* m_arrows;
+  std::vector<osg::ref_ptr<osg::Node>>* m_boxes;
+
+ public:
+  explicit MyKeyboardHandler(std::vector<osg::ref_ptr<osg::Node>>* arrows,
+                             std::vector<osg::ref_ptr<osg::Node>>* boxes)
+      : m_arrows(arrows), m_boxes(boxes) {}
+
+  virtual bool keyDown(int key) {
+    typedef osgGA::GUIEventAdapter::KeySymbol osgKey;
+    switch (key) {
+      case osgKey::KEY_F1:
+        for (size_t i = 0; i < m_arrows->size(); i++)
+          m_arrows->at(i)->setNodeMask(~m_arrows->at(i)->getNodeMask());
+        return true;
+      case osgKey::KEY_F2:
+        for (size_t i = 0; i < m_boxes->size(); i++)
+          m_boxes->at(i)->setNodeMask(~m_boxes->at(i)->getNodeMask());
+        return true;
+      default:
+        return false;
+    }
   }
 };
 
@@ -255,9 +327,10 @@ int main(int argc, char** argv) {
   osg::ArgumentParser args(&argc, argv);
 
   float radius = 12.7;
-  // float radius = 5.7;
   float value;
-  if (args.read("-r", value)) { radius = value; }
+  if (args.read("-r", value)) {
+    radius = value;
+  }
 
   OSGVoxelSphere sphere(radius);
 
@@ -269,5 +342,7 @@ int main(int argc, char** argv) {
   viewer.setSceneData(sphere.m_root);
   viewer.setUpViewInWindow(400, 400, 800, 600);
   viewer.addEventHandler(new osgViewer::StatsHandler);
+  viewer.addEventHandler(
+      new MyKeyboardHandler(sphere.m_arrows, sphere.m_boxes));
   return viewer.run();
 }
