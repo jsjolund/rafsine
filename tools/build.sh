31ed8480a50417dc4dc061b66f36ebb8811597a8
Ubuntu <ubuntu@p01r03srv19.maas>
2020-12-09 10:52:39 +0100

    Fix typo

--- tools/build.sh
+++ tools/build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-python lbm_gen_ddqq.py ../src/sim/DdQq.cu
-python lbm_gen_ddqq_idx.py ../include/DdQqIndexing.hpp
+python3 lbm_gen_ddqq.py ../src/sim/DdQq.cu
+python3 lbm_gen_ddqq_idx.py ../include/DdQqIndexing.hpp
 
 python3 lbm_gen_init.py ../src/kernel/InitKernel.cu
 python3 lbm_gen_double_bgk_les_boussinesq.py ../include/LBM_BGK.hpp
 python3 lbm_gen_double_mrt_les_boussinesq.py ../include/LBM_MRT.hpp
+python3 lbm_gen_d3q27_mrt.py ../include/LBM_MRT27.hpp
